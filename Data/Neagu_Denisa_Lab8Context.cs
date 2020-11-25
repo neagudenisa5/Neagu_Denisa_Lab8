94d7fe15ea0dca4af23afd942438dfbe8f023650
Denisa Neagu <neagudenisa140@gmail.com>
2020-11-25 15:17:28 +0200

    Add project files.

---
+++ Data/Neagu_Denisa_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Neagu_Denisa_Lab8.Models;
+
+namespace Neagu_Denisa_Lab8.Data
+{
+    public class Neagu_Denisa_Lab8Context : DbContext
+    {
+        public Neagu_Denisa_Lab8Context (DbContextOptions<Neagu_Denisa_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Neagu_Denisa_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Neagu_Denisa_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Neagu_Denisa_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Neagu_Denisa_Lab8.Data;
+using Neagu_Denisa_Lab8.Models;
+
+namespace Neagu_Denisa_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Neagu_Denisa_Lab8.Data.Neagu_Denisa_Lab8Context _context;
+
+        public IndexModel(Neagu_Denisa_Lab8.Data.Neagu_Denisa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
